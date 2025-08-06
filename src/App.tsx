d69271d785d66fef05000c81a469043be9e42cc6
veerptech <veerptech@gmail.com>
2025-08-06 17:41:50 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { CartProvider } from "@/contexts/CartContext";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import ProductList from "./components/ProductList";
+import ProductManager from "@/admin/Admin";
+import AdminDashboard from "./pages/AdminDashboard";
+import Register from "@/pages/Register";
+import Login from "@/pages/Login";
+import CheckoutPage from "./pages/CheckoutPage";
+
+
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <CartProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+
+              <Route path="/products" element={<ProductList />} />
+              
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              
+              
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </CartProvider>
+    </AuthProvider>
+  </QueryClientProvider>
+);
+
+export default App;
